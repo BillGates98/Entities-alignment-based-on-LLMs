6c9b4d848b86aef15ed9ca4d9b7ee6eda78a64a4
BillGates98 <billhappi@gmail.com>
2024-02-28 17:48:49 +0100

    WIP

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# self.models = {
+#             "mistral": ["mistralai/Mistral-7B-v0.1", "./dir_models/mistral/Mistral-7B-v0.1"],
+#             "qwen": ["Qwen/Qwen1.5-7B-Chat", "./dir_models/qwen/Qwen1.5-7B-Chat"],
+#             "bloom": ["bigscience/bloom-7b1", "./dir_models/bloom/bloom-7b1"],
+#             "gpt": ["openai-community/gpt2", "./dir_models/gpt/gpt2"]
+#         }
+
 
 class LLM:
 
     def __init__(self, model_name=None):
         self.model_name = model_name
         self.models = {
-            "mistral": ["mistralai/Mistral-7B-v0.1", "./dir_models/mistral/Mistral-7B-v0.1"],
-            "qwen": ["Qwen/Qwen1.5-7B-Chat", "./dir_models/qwen/Qwen1.5-7B-Chat"],
-            "bloom": ["bigscience/bloom-7b1", "./dir_models/bloom/bloom-7b1"],
-            "gpt": ["openai-community/gpt2", "./dir_models/gpt/gpt2"]
+            "mistral": ["mistralai/Mistral-7B-v0.1", "./dir_models/mistral"],
+            "qwen": ["Qwen/Qwen1.5-7B-Chat", "./dir_models/qwen"],
+            "bloom": ["bigscience/bloom-7b1", "./dir_models/bloom"],
+            "gpt": ["openai-community/gpt2", "./dir_models/gpt"]
         }
 
     def mistral(self):
